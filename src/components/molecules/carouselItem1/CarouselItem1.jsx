4014d8c70f5e44ec00c1fcfefcbad8a67c3ceb0c
Daniel Mayo <daniielmayo.mayo@gmail.com>
2022-10-03 11:05:48 -0500

    add views for each carrousel component

--- src/components/molecules/carouselItem1/CarouselItem1.jsx
+++ src/components/molecules/carouselItem1/CarouselItem1.jsx
@@ -2,8 +2,15 @@ import "./CarouselItem1.css"
 
 export default function CarouselItem1() {
     return(
-        <div>
-            Hola
-        </div>
+       
+        <section className="section">
+            <img   className="section-img-one" src='https://i.imgur.com/tqqRire.png' alt="" />
+            <img  className="section-img-two" src="https://i.ibb.co/pZL0RjN/Image-4.png" alt="" />
+            <div className="text">
+                <h1><span className="color-green">Regresemos a </span ><span className="color-yellow">nuestra </span><span className="color-green">casa</span></h1>
+                <p className="text-paragraf">un curso que cambiará tu vida y tú relación con el Creador</p>
+             </div>
+        </section>
+        
     )
 }
